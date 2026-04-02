f39aefb7455f104f03a825c206b4bb924918a764
Hayden Riddiford <haydenridd@gmail.com>
2026-04-02 14:02:46 -0700

    - Aaaaaaaand another

--- example/build.zig
+++ example/build.zig
@@ -45,7 +45,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .link_libc = false,
         .single_threaded = true,
-        .sanitize_c = false, // Currently important if including any C files b/c of https://github.com/ziglang/zig/issues/23052, otherwise binary can get bloated
+        .sanitize_c = .off, // Currently important if including any C files b/c of https://github.com/ziglang/zig/issues/23052, otherwise binary can get bloated
     });
 
     const blinky_exe = b.addExecutable(.{
